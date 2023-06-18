68184c7fbad7155c7bffa385ca3417a3720dda22
VadymMiezin <vadym.miezin@gmail.com>
2023-06-18 12:20:01 +0300

    fix some bugs

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
-import * as API from '../../utils/api/apiMovieDB';
-import { useState, useEffect } from 'react';
-import { TrendingList } from 'components/TrendingList/TrendingList';
 
 export const SearchForm = () => {
-  const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSubmit = e => {
@@ -14,20 +10,12 @@ export const SearchForm = () => {
     return;
   };
 
-  useEffect(() => {
-    const movieTitle = searchParams.get('query');
-    if (movieTitle) {
-      API.searchMovies(movieTitle).then(data => setMovies(data));
-    }
-  }, [searchParams]);
-
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Type title for search"></input>
         <button type="submit">Search</button>
       </form>
-      <TrendingList movies={movies} />
     </div>
   );
 };
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import * as API from '../utils/api/apiMovieDB';
+import { TrendingList } from 'components/TrendingList/TrendingList';
 import { SearchForm } from 'components/SearchForm/SearchForm';
 
 const Movies = () => {
-  return <SearchForm />;
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const movieTitle = searchParams.get('query');
+    if (movieTitle) {
+      API.searchMovies(movieTitle).then(data => setMovies(data));
+    }
+  }, [searchParams]);
+
+  return (
+    <div>
+      <SearchForm />
+      <TrendingList movies={movies} />
+    </div>
+  );
 };
 
 export default Movies;
